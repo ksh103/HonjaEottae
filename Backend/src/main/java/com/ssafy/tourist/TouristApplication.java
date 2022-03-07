b78dbeb355b54587829f456979f1ba82e7c6d2b5
강소현 <jayy_z@naver.com>
2022-03-07 10:18:19 +0900

    BE : 환경설정

---
+++ Backend/src/main/java/com/ssafy/tourist/TouristApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.tourist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouristApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TouristApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/ssafy/tourist/TouristApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.tourist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TouristApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
