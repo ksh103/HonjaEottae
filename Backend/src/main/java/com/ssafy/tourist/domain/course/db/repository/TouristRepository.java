ed24c3663ae6df02f2f713e04f31d3387e4872fe
강소현 <jayy_z@naver.com>
2022-03-23 14:14:20 +0900

    [S06P22E103-83] Refactor : 인근 반경 km 수정

--- Backend/src/main/java/com/ssafy/tourist/domain/course/db/repository/TouristRepository.java
+++ Backend/src/main/java/com/ssafy/tourist/domain/course/db/repository/TouristRepository.java
@@ -10,7 +10,7 @@ public interface TouristRepository extends JpaRepository<Tourist, Integer> {
 
     @Query(value = "select *, (6371*acos(cos(radians(:lat))*cos(radians(tourist_lat))*cos(radians(tourist_lng)-radians(:lng))+sin(radians(:lat))*sin(radians(tourist_lat)))) AS distance " +
             "FROM tourist " +
-            "HAVING distance <= 1 " +
+            "HAVING distance <= 2 " +
             "ORDER BY distance " +
             "LIMIT 5", nativeQuery = true)
     List<Tourist> findLocationTouristBy(double lat, double lng);
