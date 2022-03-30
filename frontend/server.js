6e373bf4c1753fa7335449b7e6dafdf3832a725a
차상훈 <sanghunc62@gmail.com>
2022-03-31 01:41:16 +0900

    [S06P22E103-224] Fix:nginx 에러 로그 분석 및 수정

--- frontend/server.js
+++ frontend/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const http = require('http');
+const https = require('https');
 const socketIO = require('socket.io');
 
 // localhost 포트 설정
@@ -8,7 +8,7 @@ const port = 4002;
 const app = express();
 
 // server instance
-const server = http.createServer(app);
+const server = https.createServer(app);
 
 // socketio 생성후 서버 인스턴스 사용
 const io = socketIO(server);
