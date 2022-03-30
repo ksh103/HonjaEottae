e526940227ef05e657f9856ec5ea0bc8b44440a5
차상훈 <sanghunc62@gmail.com>
2022-03-30 22:21:32 +0900

    [S06P22E103-207] Feat:Flask Server API 통신 테스트

--- flask_server/app/app.py
+++ flask_server/app/app.py
@@ -1,10 +1,9 @@
 from flask import Flask
 
 import test
-
+import ssl
 
 app = Flask(__name__)
-
 app.debug = True
 
 
@@ -14,4 +13,7 @@ def index():
 
 
 if __name__ == '__main__':
-  app.run(host='http://localhost', port=5000, debug=True)
+    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS)
+    ssl_context.load_cert_chain(certfile='/root/cert.pem', keyfile='/root/privkey.pem', password='ssafy')
+    app.run(host="0.0.0.0", port=5000, ssl_context=ssl_context, debug=True)
+    #app.run(host='0.0.0.0', port=5000, debug=True, ssl_context=ssl_context)
