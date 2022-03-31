6ede79d387149c0d4f9be80657b6e3d682665cbc
진은정 <1104_jin@naver.com>
2022-03-31 13:39:13 +0900

    [S06P22E103-174] Fix : 방문 관광지코스명 추가

--- Backend/src/main/java/com/ssafy/tourist/domain/user/db/bean/CourseNameVisitDetail.java
+++ Backend/src/main/java/com/ssafy/tourist/domain/user/db/bean/CourseNameVisitDetail.java
@@ -7,6 +7,7 @@ import lombok.Data;
 @AllArgsConstructor
 public class CourseNameVisitDetail {
     private int courseId;
+    private String courseName;
     private double touristLat;
     private double touristLng;
 }
--- Backend/src/main/java/com/ssafy/tourist/domain/user/db/repository/UserRepositorySpp.java
+++ Backend/src/main/java/com/ssafy/tourist/domain/user/db/repository/UserRepositorySpp.java
@@ -2,6 +2,7 @@ package com.ssafy.tourist.domain.user.db.repository;
 
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.ssafy.tourist.domain.course.db.entity.QCourse;
 import com.ssafy.tourist.domain.course.db.entity.QCourseData;
 import com.ssafy.tourist.domain.course.db.entity.QTourist;
 import com.ssafy.tourist.domain.record.db.entity.QTour;
@@ -25,6 +26,7 @@ public class UserRepositorySpp {
     QCourseData qCourseData = QCourseData.courseData;
     QTourist qTourist = QTourist.tourist;
     QTourStamp qTourStamp = QTourStamp.tourStamp;
+    QCourse qCourse = QCourse.course;
 
     public User findByEmail(String userEmail){
         return jpaQueryFactory.select(quser).from(quser)
@@ -34,10 +36,11 @@ public class UserRepositorySpp {
     //방문한 사용자 코스 데이터 Query
     public List<CourseNameVisitDetail> courseVisitDetailByUserId(int userId){
         return jpaQueryFactory.select(Projections.constructor(CourseNameVisitDetail.class,
-                        qTour.courseId.as("courseId"), qTourist.touristLat.as("touristLat")
+                        qTour.courseId.as("courseId"), qCourse.courseName.as("courseName"), qTourist.touristLat.as("touristLat")
                         ,qTourist.touristLng.as("touristLng")))
                 .from(qTour).join(qCourseData).on(qCourseData.courseId.eq(qTour.courseId))
                 .join(qTourist).on(qTourist.touristId.eq(qCourseData.touristId))
+                .join(qCourse).on(qCourse.courseId.eq(qTour.courseId))
                 .where(qTour.isEnd.eq(true).and(qTour.userId.eq(userId)).and(qCourseData.courseDataId.eq(1))).fetch();
 
     }
