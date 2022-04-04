bc2ecbed6766f9865829729dea388570d1ca9e7c
손은성 <dmstjd3256@naver.com>
2022-04-04 09:51:24 +0900

    [S06P22E103-77] Feat : socekt 채팅 server 설정

--- socket/server.js
+++ socket/server.js
@@ -1,14 +1,14 @@
-const fs = require("fs");
-const httpServer = require("https").createServer({
-  key: fs.readFileSync("/var/www/html/privkey.pem"),
-  cert: fs.readFileSync("/var/www/html/cert.pem")
-});
+const fs = require('fs')
+const httpServer = require('https').createServer({
+  key: fs.readFileSync('/var/www/html/privkey.pem'),
+  cert: fs.readFileSync('/var/www/html/cert.pem'),
+})
 const options = {
-  key: fs.readFileSync("/var/www/html/privkey.pem"),
-  cert: fs.readFileSync("/var/www/html/cert.pem")
- };
+  key: fs.readFileSync('/var/www/html/privkey.pem'),
+  cert: fs.readFileSync('/var/www/html/cert.pem'),
+}
 
-const io = require("socket.io")(httpServer, options);
+const io = require('socket.io')(httpServer, options)
 //const io = require('socket.io')(httpServer, {
 //  cors: {
 //    origin: '*',
@@ -16,18 +16,33 @@ const io = require("socket.io")(httpServer, options);
 //  },
 //});
 
-
 // socketio 문법
 io.on('connection', socket => {
-  console.log('user connections: ', socket.id);
-  socket.on('send message', item => {
-    const msg = item.name + ' : ' + item.message;
-    console.log(msg);
-    io.emit('receive message', { name: item.name, message: item.message });
-  });
-  socket.on('disconnect', function () {
-    console.log('user disconnected: ', socket.id);
-  });
-});
+  console.log('user connections: ', socket.id)
 
-httpServer.listen(4003);
+  socket.on('disconnect', function () {
+    console.log('user disconnected: ', socket.id)
+  })
+
+  socket.on('leaveRoom', (num, name) => {
+    console.log(name + 'leave a ' + `room${num}`)
+    socket.leave(`room${num}`, () => {
+      console.log(name + 'leave a ' + `room${num}`)
+      io.to(`room${num}`).emit('leaveRoom', num, name)
+    })
+  })
+
+  socket.on('joinRoom', (num, name) => {
+    console.log('조인', num, name)
+    socket.join(`room${num}`, () => {
+      console.log(name + ' join a ' + `room${num}`)
+      io.to(`room${num}`).emit('joinRoom', num, name)
+    })
+  })
+
+  socket.on('chat message', (num, name, msg) => {
+    io.to(`room${num}`).emit('chat message', { name: name, message: msg })
+  })
+})
+
+httpServer.listen(4003)
